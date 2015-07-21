805ca48d8845482f5d1e93c53855404345614bea
Bhagwat Kumar <bhagwat@intelligrape.com>
2015-07-21 08:59:23 +0530

    added more utility methods

--- bin/common.sh
+++ bin/common.sh
@@ -16,3 +16,16 @@ get_property()
 indent() {
   sed -u 's/^/       /'
 }
+
+export_env_dir() {
+  env_dir=$3
+  whitelist_regex=${2:-''}
+  blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  if [ -d "$env_dir" ]; then
+    for e in $(ls $env_dir); do
+      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      export "$e=$(cat $env_dir/$e)"
+      :
+    done
+  fi
+}
